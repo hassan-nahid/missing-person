4fcbe48823215a68eda31edf5792b4890cae9b01
Nahid-subi <scientisthassannahid@gmail.com>
2025-01-28 15:45:09 +0600

    some bug fixed

--- frontend/src/components/shared/Card.jsx
+++ frontend/src/components/shared/Card.jsx
@@ -8,35 +8,39 @@ const Card = ({ data, status }) => {
     month: "long",
     day: "numeric",
   });
+
   return (
     <div className="card bg-gray-50 shadow-md rounded-lg overflow-hidden">
       <div className="relative">
         <img
           src={photo || noPhoto}  // If no photo, use a default image
           alt="Post"
-          className="w-full h-64 object-cover rounded-t-lg"
+          className="w-full h-72 object-cover rounded-t-lg"
         />
         {/* Optional: Add overlay or border effect */}
       </div>
       <div className="p-4">
-        <h4 className="text-xl font-semibold text-gray-800">
+        <h4 className="text-xl font-semibold text-gray-800 truncate">
           {status}: {name}
         </h4>
         {lastSeen && (
-          <p className="text-gray-500 mt-2">
+          <p className="text-gray-500 mt-2 truncate">
             <span className="font-semibold">Last seen:</span> {lastSeen}
           </p>
         )}
 
         {foundAt && (
-          <p className="text-gray-500 mt-2">
+          <p className="text-gray-500 mt-2 truncate">
             <span className="font-semibold">Found at:</span> {foundAt}
           </p>
         )}
         <p className="text-gray-500 mt-1">
           <span className="font-semibold">Date:</span> {formattedDate}
         </p>
-        <Link to={status === "Missing" ? `/missing_post_details/${_id}` : `/found_post_details/${_id}`} className="btn btn-sm blue-bg hover:blue-bg text-white w-full mt-4 py-2 rounded">
+        <Link
+          to={status === "Missing" ? `/missing_post_details/${_id}` : `/found_post_details/${_id}`}
+          className="btn btn-sm blue-bg hover:blue-bg text-white w-full mt-4 py-2 rounded"
+        >
           View Details
         </Link>
       </div>

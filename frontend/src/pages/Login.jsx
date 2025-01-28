6c627dcd03f4a41b8b630dc6f835ca4b4117d08e
Nahid-subi <scientisthassannahid@gmail.com>
2025-01-28 19:19:07 +0600

    login and register page style updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -71,7 +71,7 @@ const Login = () => {
 
   return (
     <div>
-      <div className="hero bg-base-200">
+      <div className="hero bg-white min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse w-full">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form onSubmit={handleLogin} className="card-body">
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -66,7 +66,7 @@ const Register = () => {
 
   return (
     <div className="">
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="hero bg-white min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse w-full">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form onSubmit={handleRegister} className="card-body">
